22b4169f9cc7c735d8ff41a9e03b430520d113a9
Maryan Shutov <maryanshutov@gmail.com>
2019-05-10 15:21:14 +0000

    Changed

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const clockContainer = document.querySelector('.clock');
+setInterval( () => clockContainer.innerText = (new Date()).toLocaleTimeString(), 1000);
+
+const dateContainer = document.querySelector('.calendar');
+setInterval( () => dateContainer.innerText = (new Date()).toLocaleDateString(), 1000);
+
